060d85ba7708ca088f1dbdd8004e6bb2b962d396
Adrian Heinz <adrianiespuigdesafont@gmail.com>
2022-05-30 13:31:15 +0200

    Avançament Javascript, funciona pero desapareix UL-HEADER després de pitjar el botó del menú, i el tornes a redimensionar a mes gran

---
+++ script.js
@@ -0,0 +1,13 @@
+let headerul = document.getElementById("header-ul");
+
+let menu = document.getElementById("menu");
+
+
+const show = () => {
+
+    if (headerul.style.display === "none") {
+        headerul.style.display = "block";
+      } else {
+        headerul.style.display = "none";
+      }
+  };
