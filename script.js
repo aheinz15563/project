baf0d736b2a16235120b5ae5945a8546c8cb398b
Adrian Heinz <adrianiespuigdesafont@gmail.com>
2022-06-03 13:07:41 +0200

    Arreglat Problemes de Posicionament, Introduït Pagines Externes, Login, Contact, Register

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ const show = () => {
         headerul.style.display = "none";
       }
   };
+
+var mql = window.matchMedia('(max-width: 800px)');
+
+function screenTest(e) {
+  if (e.matches) {
+    /* the viewport is 800 pixels wide or less */
+    headerul.style.display = "none";
+
+  } else {
+    headerul.style.display = "flex";
+
+  }
+}
+
+mql.addListener(screenTest);
